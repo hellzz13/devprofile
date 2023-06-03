ea5c6f3262fc6694386c5d98e3d4142d64ec25d7
hellzz13 <hpalenca13@gmail.com>
2023-06-03 00:39:31 -0300

    configurando styled components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Conteiner, Title } from './styles';
+
+export const Home: React.FunctionComponent = () => {
+    return (
+        <Conteiner>
+            <Title>Home Page</Title>
+        </Conteiner>
+    );
+};
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Conteiner = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #121214;
+`;
+
+export const Title = styled.Text`
+    font-size: 24;
+    font-weight: bold;
+    color: #f1f1f1;
+`;
